adfc651ed546e720d519b01d187bd2a7eceb62d1
Emad Hedayati <hedayati.emad@gmail.com>
2022-06-28 09:37:48 +0430

    bug fix

--- lib/store_checker.dart
+++ lib/store_checker.dart
@@ -8,7 +8,7 @@ class StoreChecker {
   static const MethodChannel _channel = const MethodChannel('store_checker');
 
   /* Get origin of installed apk/ipa */
-  static Future<String?> get getSource async {
+  static Future<String?> getSource() async {
     final String? sourceName = await _channel.invokeMethod('getSource');
     return sourceName;
   }
